edf4b4e7964c7923aec15cbab3e372f476717495
Clément VANDENDAELEN <cv@icure.com>
2022-08-17 10:06:34 +0200

    Added 2FA features

--- src/main/kotlin/org/taktik/icure/security/UnauthorizedEntryPoint.kt
+++ src/main/kotlin/org/taktik/icure/security/UnauthorizedEntryPoint.kt
@@ -7,11 +7,17 @@ import org.springframework.http.HttpStatus
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.server.ServerAuthenticationEntryPoint
 import org.springframework.web.server.ServerWebExchange
+import org.taktik.icure.exceptions.Invalid2FAException
+import org.taktik.icure.exceptions.Missing2FAException
 import reactor.core.publisher.Mono
 
-class Http401UnauthorizedEntryPoint : ServerAuthenticationEntryPoint {
+class UnauthorizedEntryPoint() : ServerAuthenticationEntryPoint {
 	override fun commence(exchange: ServerWebExchange, e: AuthenticationException): Mono<Void> {
-		exchange.response.statusCode = HttpStatus.UNAUTHORIZED
+		exchange.response.statusCode = when (e) {
+			is Missing2FAException -> HttpStatus.EXPECTATION_FAILED
+			is Invalid2FAException -> HttpStatus.NOT_ACCEPTABLE
+			else -> HttpStatus.UNAUTHORIZED
+		}
 		return Mono.empty()
 	}
 }
