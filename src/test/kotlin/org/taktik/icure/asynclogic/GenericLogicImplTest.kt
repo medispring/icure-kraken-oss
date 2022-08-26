adf6ca51a61aa78cb27ca577ea847b7f295c4651
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2022-08-26 11:02:43 +0200

    ICBE-27 #comment updated GenericLogicImplTest using mockk

--- src/test/kotlin/org/taktik/icure/asynclogic/GenericLogicImplTest.kt
+++ src/test/kotlin/org/taktik/icure/asynclogic/GenericLogicImplTest.kt
@@ -5,15 +5,23 @@ import java.util.UUID
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.count
 import org.junit.jupiter.api.TestInstance
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
+import org.taktik.icure.asyncdao.MaintenanceTaskDAO
+import org.taktik.icure.asynclogic.impl.MaintenanceTaskLogicImpl
 import org.taktik.icure.entities.MaintenanceTask
+import org.taktik.icure.asynclogic.impl.filter.Filters
 import org.taktik.icure.test.ICureTestApplication
 
 private const val TEST_CACHE = "build/tests/icureCache"
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @SpringBootTest(
 	classes = [ICureTestApplication::class],
 	properties = [
@@ -28,9 +36,14 @@ private const val TEST_CACHE = "build/tests/icureCache"
 )
 @ActiveProfiles("app")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class GenericLogicImplTest (
-	taskLogic: MaintenanceTaskLogic
-): StringSpec({
+class GenericLogicImplTest : StringSpec({
+
+	val mockDAO = mockk<MaintenanceTaskDAO> {
+		coEvery { create(any<Collection<MaintenanceTask>>()) } answers { firstArg<Collection<MaintenanceTask>>().asFlow() }
+	}
+	val mockSessionLogic = mockk<AsyncSessionLogic>()
+	val mockFilters = mockk<Filters>()
+	val taskLogic = MaintenanceTaskLogicImpl(mockDAO, mockSessionLogic, mockFilters)
 
 	"Can create some DTOs if all of them have the rev field equal to none" {
 		val entities = listOf(
@@ -39,7 +52,6 @@ class GenericLogicImplTest (
 			MaintenanceTask(id = UUID.randomUUID().toString(), rev = null),
 		)
 		taskLogic.createEntities(entities).count() shouldBe 3
-		taskLogic.getGenericDAO().getEntities(entities.map { it.id }).count() shouldBe 3
 	}
 
 	"Creation of DTOs fails if at least one of them has the rev field not equal to none" {
@@ -51,7 +63,6 @@ class GenericLogicImplTest (
 		shouldThrow<IllegalStateException> {
 			taskLogic.createEntities(entities).count()
 		}
-		taskLogic.getGenericDAO().getEntities(entities.map { it.id }).count() shouldBe 0
 	}
 
 })
