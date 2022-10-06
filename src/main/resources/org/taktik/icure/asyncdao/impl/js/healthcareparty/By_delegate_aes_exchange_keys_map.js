1b25dfa74cf889e0ca032d0e19cc84f1841befa7
Antoine Duchâteau <ad@taktik.com>
2022-10-06 16:26:13 +0200

    Fix views for old Couchdb

--- src/main/resources/org/taktik/icure/asyncdao/impl/js/healthcareparty/By_delegate_aes_exchange_keys_map.js
+++ src/main/resources/org/taktik/icure/asyncdao/impl/js/healthcareparty/By_delegate_aes_exchange_keys_map.js
@@ -1,4 +1,4 @@
-map = function(doc) {
+map = function (doc) {
 	var emitted = []
 	if (doc.java_type === 'org.taktik.icure.entities.HealthcareParty' && !doc.deleted && ((doc.hcPartyKeys && doc.publicKey) || doc.aesExchangeKeys)) {
 		var aesPubKeys = Object.keys(doc.aesExchangeKeys || {});
@@ -8,16 +8,18 @@ map = function(doc) {
 				var delegateKeys = ks[hcpId];
 				Object.keys(delegateKeys).forEach(function (delPub) {
 					var delK = delegateKeys[delPub]
-					if (pk === doc.publicKey) { emitted.push(hcpId) }
+					if (pk === doc.publicKey) {
+						emitted.push(hcpId)
+					}
 					emit(hcpId, [doc._id, pk.slice(-32), delPub.slice(-32), delK]);
 				})
 			});
 		});
 
-			Object.keys(doc.hcPartyKeys).forEach(function (k) {
-		  if (!emitted.includes(k)) {
+		Object.keys(doc.hcPartyKeys).forEach(function (k) {
+			if (!emitted.includes(k)) {
 				emit(k, [doc._id, doc.publicKey.slice(-32), '', doc.hcPartyKeys[k][1]]);
-		  }
-			});
-		}
+			}
+		});
 	}
+}
