e32957918d1ae39719cb222595228cac4f59ca7e
Antoine Duchâteau <ad@taktik.com>
2022-11-13 20:59:32 +0100

    Allow for session bypass through header
    (cherry picked from commit d7e40e190a667184446efd31bec9d24410082e35)

--- src/main/kotlin/org/taktik/icure/config/SessionConfig.kt
+++ src/main/kotlin/org/taktik/icure/config/SessionConfig.kt
@@ -12,7 +12,11 @@ import org.springframework.session.MapSession
 import org.springframework.session.ReactiveMapSessionRepository
 import org.springframework.session.ReactiveSessionRepository
 import org.springframework.session.config.annotation.web.server.EnableSpringWebSession
+import org.springframework.session.web.server.session.SpringSessionWebSessionStore
+import org.springframework.web.server.ServerWebExchange
 import org.springframework.web.server.session.CookieWebSessionIdResolver
+import org.springframework.web.server.session.DefaultWebSessionManager
+import reactor.core.publisher.Mono
 
 @Configuration
 @ConditionalOnProperty(prefix = "spring", name = ["session.enabled"], havingValue = "true", matchIfMissing = false)
@@ -25,4 +29,10 @@ class SessionConfig {
 
 	@Bean
 	fun webSessionIdResolver() = CookieWebSessionIdResolver().apply { addCookieInitializer { cb -> cb.sameSite("None").secure(true) } }
+
+	@Bean
+	fun webSessionManager(repository: ReactiveSessionRepository<MapSession>) = object : DefaultWebSessionManager() {
+		override fun getSession(exchange: ServerWebExchange) = exchange.request.headers["X-Bypass-Session"]?.let { Mono.empty() } ?: super.getSession(exchange)
+	}.apply<DefaultWebSessionManager> { sessionStore = SpringSessionWebSessionStore(repository) }
+
 }
