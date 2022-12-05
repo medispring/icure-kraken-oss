38bfb58c87a83d7940d35f52a0b6e33961a530a7
LAPSAM6\sjocq <sam.jocque@medispring.be>
2022-12-05 14:48:12 +0100

    Merge branch 'feature/MS-8320-better-address-search' of https://github.com/medispring/icure-kraken-oss into feature/MS-8320-patient-by-address-search
    # Conflicts:
    #	src/main/kotlin/org/taktik/icure/asyncdao/PatientDAO.kt
    #	src/main/kotlin/org/taktik/icure/asynclogic/PatientLogic.kt
    #	src/main/kotlin/org/taktik/icure/asynclogic/impl/filter/patient/PatientByHcPartyAndAddressFilter.kt

--- src/main/kotlin/org/taktik/icure/asyncdao/impl/PatientDAOImpl.kt
+++ src/main/kotlin/org/taktik/icure/asyncdao/impl/PatientDAOImpl.kt
@@ -264,8 +264,8 @@ class PatientDAOImpl(
 		emitAll(client.queryView<Array<String>, String>(viewQuery).mapNotNull { it.value })
 	}
 
-	@View(name = "by_hcparty_address_postalcode_housenumber", map = "classpath:js/patient/By_hcparty_address_postalcode_housenumber.js")
-	override fun listPatientIdsByHcPartyAndAddress(searchString: String?, postalCode: String?, houseNumber: String?, healthcarePartyId: String): Flow<String> = flow {
+	@View(name = "by_hcparty_address", map = "classpath:js/patient/By_hcparty_address.js")
+	override fun listPatientIdsByHcPartyAndAddress(searchString: String?, healthcarePartyId: String): Flow<String> = flow {
 		val client = couchDbDispatcher.getClient(dbInstanceUrl)
 		val startKey: ComplexKey
 		val endKey: ComplexKey
@@ -279,10 +279,9 @@ class PatientDAOImpl(
 			endKey = ComplexKey.of(healthcarePartyId, "\ufff0")
 		}
 
-		val viewQuery = createQuery(client, "by_hcparty_address_postalcode_housenumber").startKey(startKey).endKey(endKey).includeDocs(false)
-		emitAll(client.queryView<Array<String>, String>(viewQuery).filter {
-			(houseNumber.isNullOrEmpty() || it.key?.get(3) == houseNumber) &&
-			(postalCode.isNullOrEmpty() || it.key?.get(2) == postalCode) }.mapNotNull { it.value })
+		val viewQuery = createQuery(client, "by_hcparty_address").startKey(startKey).endKey(endKey).includeDocs(false)
+
+		emitAll(client.queryView<Array<String>, String>(viewQuery).mapNotNull { it.value })
 	}
 
 	@View(name = "by_hcparty_address_postalcode_housenumber", map = "classpath:js/patient/By_hcparty_address_postalcode_housenumber.js")
