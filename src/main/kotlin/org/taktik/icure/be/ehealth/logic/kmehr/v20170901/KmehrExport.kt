e0860fadcf2e7b2e9c884b451df030673918f184
Dylan Friedrich <dylan.friedrich@medispring.be>
2023-01-18 09:46:47 +0100

    map patient gender enum to cd-sex

--- src/main/kotlin/org/taktik/icure/be/ehealth/logic/kmehr/v20170901/KmehrExport.kt
+++ src/main/kotlin/org/taktik/icure/be/ehealth/logic/kmehr/v20170901/KmehrExport.kt
@@ -144,6 +144,7 @@ import org.taktik.icure.entities.base.Code
 import org.taktik.icure.entities.base.CodeStub
 import org.taktik.icure.entities.embed.Address
 import org.taktik.icure.entities.embed.Content
+import org.taktik.icure.entities.embed.Gender
 import org.taktik.icure.entities.embed.PlanOfAction
 import org.taktik.icure.entities.embed.Service
 import org.taktik.icure.utils.FuzzyValues
@@ -249,9 +250,20 @@ open class KmehrExport(
 			p.id.let { id -> ids.add(IDPATIENT().apply { s = IDPATIENTschemes.LOCAL; sv = config.soft?.version; sl = "${config.soft?.name}-Person-Id"; value = id }) }
 			firstnames.add(p.firstName)
 			familyname = p.lastName
-			sex = SexType().apply { cd = CDSEX().apply { s = "CD-SEX"; sv = "1.0"; value = p.gender?.let { CDSEXvalues.fromValue(it.name) } ?: CDSEXvalues.UNKNOWN } }
+			sex = SexType().apply { cd = CDSEX().apply { s = "CD-SEX"; sv = "1.0"; value = p.gender?.let { genderToSexMapper(it) } ?: CDSEXvalues.UNKNOWN } }
 			p.dateOfBirth?.let { birthdate = Utils.makeDateTypeFromFuzzyLong(it.toLong()) }
 			recorddatetime = makeXGC(p.modified)
+		}
+	}
+
+	private fun genderToSexMapper(gender: Gender): CDSEXvalues{
+		return when(gender){
+			Gender.changed,Gender.changedToMale,Gender.changedToFemale -> CDSEXvalues.UNDEFINED
+			Gender.male -> CDSEXvalues.MALE
+			Gender.female -> CDSEXvalues.FEMALE
+			Gender.indeterminate -> CDSEXvalues.UNDEFINED
+			Gender.unknown -> CDSEXvalues.UNKNOWN
+			else -> CDSEXvalues.UNKNOWN
 		}
 	}
 
